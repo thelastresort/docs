fe312ebc155a72008655e875688b42d4b69d8097
jammychan <chenzemin23@gmail.com>
2016-05-03 00:52:20 +0800

    handler message初稿-demo代码

--- AndroidDemo/src/com/flamingo/demo/guopan/MainActivity.java
+++ AndroidDemo/src/com/flamingo/demo/guopan/MainActivity.java
@@ -22,10 +22,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity_TAG";
     private ArrayList<String> mStringList = new ArrayList<>();
+    private Message mMessage = null;
+
 
     private static final int MSG_MULTI_THREAD_ACCESS_DATA = 1;
+    private static final int MSG_USE_USED_MESSAGE = 2;
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message message) {
@@ -33,11 +36,15 @@ public class MainActivity extends Activity {
                 case MSG_MULTI_THREAD_ACCESS_DATA:
                     handleMultiThreadDataMessage();
                     break;
+                case MSG_USE_USED_MESSAGE:
+                    handleUseUsedMessage(message);
+                    break;
                 default:
                     break;
             }
         }
     };
+
 
     /**
      * 主线程处理数据（数据是其他线程已经处理好的）
@@ -48,13 +55,21 @@ public class MainActivity extends Activity {
         }
     }
 
+
+    private void handleUseUsedMessage(Message msg) {
+        Log.i(TAG, String.format("msg addr=%d, msg what=%d, msg arg1=%d", msg.hashCode(), msg.what, msg.arg1));
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn_multi_thread_access_data).setOnClickListener(listener);
+        findViewById(R.id.btn_use_used_message).setOnClickListener(listener);
     }
+
 
     private OnClickListener listener = new OnClickListener() {
         @Override
@@ -63,11 +78,35 @@ public class MainActivity extends Activity {
                 case R.id.btn_multi_thread_access_data:
                     onClickMultiThreadAccessData();
                     break;
+                case R.id.btn_use_used_message:
+                    onClickUsedMessage();
+                    break;
                 default:
                     break;
             }
         }
     };
+
+
+    private void onClickUsedMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<5; ++i) {
+                    mMessage = Message.obtain();
+                    mMessage.what = MSG_USE_USED_MESSAGE;
+                    mMessage.arg1 = i;
+                    mHandler.sendMessage(mMessage);
+                    try {
+                        Log.i(TAG, "************ onClickUsedMessage sleep for some time ***********");
+                        Thread.sleep(5000); // 试试这里改成1毫秒
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
 
 
     private void onClickMultiThreadAccessData() {
@@ -76,11 +115,12 @@ public class MainActivity extends Activity {
             public void run() {
                 while (true) {
                     try {
-                        Log.i(TAG, "************ sleep for some time ***********");
+                        Log.i(TAG, "************ onClickMultiThreadAccessData sleep for some time ***********");
                         Thread.sleep(1000); // 试试这里改成1毫秒
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    Message.obtain();
                     mStringList.clear();
                     for (int i = 0; i < 10; ++i) {
                         mStringList.add("str" + i);
@@ -97,6 +137,7 @@ public class MainActivity extends Activity {
     private void showToast(String text) {
         Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
+
 
     public static void show(Context context, CharSequence text) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
